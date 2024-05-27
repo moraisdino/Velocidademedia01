71ab88523315f13e98a5e1c96433cd2e93458cdf
José Gabriel Morais <169068802+moraisdino@users.noreply.github.com>
2024-05-27 20:45:42 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("programa para o calculo de velocidade media em km/h");
+    System.out.println("");
+    System.out.println(" a distancia considerada é de:");
+    double distancia = 500;
+    System.out.println("distancia:500");
+    System.out.println("");
+    System.out.println("com um total de tempo gasto de:");
+    double tempo = 7;
+    System.out.println("");
+    System.out.println("tempo:7");
+    System.out.println("A velocidade media");
+    double velocidademedia = distancia / tempo;
+    System.out.println("");
+    System.out.println("velocidade media = distancia / tempo ");
+    
+      
+    
+  }
+
+
+}
